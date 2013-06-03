6396e6b0e5cf713315ed50a3ad3b3b4be45917a0
Dolan <dolan_miu@hotmail.com>
2013-06-03 17:43:02 +0100

    Working Reverse Geocoder

--- Reverse Geocoder/MainWindow.xaml.cs
+++ Reverse Geocoder/MainWindow.xaml.cs
@@ -32,14 +32,19 @@ namespace Reverse_Geocoder
 
         private void OnLoad(object sender, RoutedEventArgs e)
         {
-            IGeoCoder geoCoder = new GoogleGeoCoder();
+            //IGeoCoder geoCoder = new GoogleGeoCoder();
             //Address[] addresses = geoCoder.ReverseGeocode(51.369479, 0.518109).ToArray();
-            ReadXLSX();
+            //Location latLang = geoCoder.GeoCode("ME1 2DQ").ToArray()[0].Coordinates;
+            //Address[] addresses = geoCoder.ReverseGeocode(latLang.Latitude, latLang.Longitude).ToArray();
+            
+
+            XSSFWorkbook workbook = LoadXLSX("AddressList.xlsx");
+            XSSFWorkbook postcodeWorkbook = ReadPostCodeXLSX();
+            DecypherPostcode(postcodeWorkbook, workbook);
+            //workbook = EditXLSX(workbook);
+            //WriteXLSX(workbook, "AddressList.xlsx");
             double[] beijingInnCoords = { 51.387155 , 0.548719 };
-            /*double xDistance = beijingInnCoords[0] - 51.516853;
-            double yDistance = beijingInnCoords[1] - (-0.188693);
-            double pythagorasDistance = Math.Sqrt(Math.Pow(xDistance, 2) + Math.Pow(yDistance, 2));
-            Distance distance = new Distance(pythagorasDistance, DistanceUnits.Miles);*/
+            
             double distance = getDistanceFromLatLonInKm(51.369479, 0.518109, beijingInnCoords[0], beijingInnCoords[1]);
         }
 
@@ -57,7 +62,7 @@ namespace Reverse_Geocoder
             return deg * (Math.PI/180);
         }
 
-        public void ReadXLSX()
+        public XSSFWorkbook ReadPostCodeXLSX()
         {
             XSSFWorkbook hssfwb;
             string fileLocation = Directory.GetCurrentDirectory() + "\\MedwayPostcodes.xlsx";
@@ -66,18 +71,93 @@ namespace Reverse_Geocoder
                 hssfwb = new XSSFWorkbook(file);
             }
 
-            ISheet sheet = hssfwb.GetSheet("Sheet1");
-            for (int row = 0; row <= sheet.LastRowNum; row++)
+            return hssfwb;
+        }
+
+        public void DecypherPostcode(XSSFWorkbook medwayPostcodes, XSSFWorkbook outputXLSX)
+        {
+            ISheet medwayPostcodeSheet = medwayPostcodes.GetSheet("Sheet1");
+            ISheet outputSheet = outputXLSX.GetSheet("Sheet1");
+            //for (int row = 1; row <= medwayPostcodeSheet.LastRowNum; row++)
+            for (int row = 202; row <= 3000; row++)
             {
-                if (sheet.GetRow(row) != null) //null is when the row only contains empty cells 
+                if (medwayPostcodeSheet.GetRow(row) != null) //null is when the row only contains empty cells 
                 {
-                    //MessageBox.Show(string.Format("Row {0} = {1}", row, sheet.GetRow(row).GetCell(0).StringCellValue));
-                    String englishName = sheet.GetRow(row).GetCell(0).StringCellValue;
-                    String chineseName = sheet.GetRow(row).GetCell(1).StringCellValue;
-                    float price = (float)sheet.GetRow(row).GetCell(2).NumericCellValue;
-                    //CreateItem(englishName, chineseName, price);
+                    String postCode = medwayPostcodeSheet.GetRow(row).GetCell(0).StringCellValue;
+                    double longPos = medwayPostcodeSheet.GetRow(row).GetCell(13).NumericCellValue;
+                    double latPos = medwayPostcodeSheet.GetRow(row).GetCell(14).NumericCellValue;
+                    System.Threading.Thread.Sleep(1000);
+                    AddStreet(outputSheet, row, postCode, latPos, longPos);
+                }
+                if (row % 100 == 0)
+                {
+                    WriteXLSX(outputXLSX, "AddressList.xlsx");
                 }
             }
+            WriteXLSX(outputXLSX, "AddressList.xlsx");
+        }
+
+        public void AddStreet(ISheet sheet, int row, string postCode, double latPos, double longPos)
+        {
+            IGeoCoder geoCoder = new GoogleGeoCoder();
+            Location latLang = geoCoder.GeoCode(postCode).ToArray()[0].Coordinates;
+            Address[] addresses = geoCoder.ReverseGeocode(latLang.Latitude, latLang.Longitude).ToArray();
+            string streetNameNumber = addresses[0].FormattedAddress.Split(',')[0];
+            bool hasHouseNumber = System.Text.RegularExpressions.Regex.IsMatch(streetNameNumber, @"\d");
+            int count = 0;
+            if (hasHouseNumber)
+            {
+                count = 1;
+            }
+
+            string[] streetNameNumberArray = addresses[0].FormattedAddress.Split(',')[0].Split(' ');
+            //string town = addresses[0].FormattedAddress.Split(',')[1].Split(' ')[0];
+            string town = addresses[0].FormattedAddress.Split(',')[1].Trim();
+            string streetName = "";
+
+            while (count < streetNameNumberArray.Length)
+            {
+                streetName += streetNameNumberArray[count];
+                if (count != streetNameNumberArray.Length - 1)
+                {
+                    streetName += " ";
+                }
+                count++;
+            }
+            sheet.CreateRow(row);
+            sheet.GetRow(row).CreateCell(0).SetCellValue(postCode);
+            sheet.GetRow(row).CreateCell(1).SetCellValue(streetName);
+            sheet.GetRow(row).CreateCell(2).SetCellValue(town);
+            Console.WriteLine(postCode + " " + streetName + " " + town);
+        }
+
+
+
+        public XSSFWorkbook LoadXLSX(string filename)
+        {
+            XSSFWorkbook hssfwb;
+            string fileLocation = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + filename;
+            using (FileStream file = new FileStream(@fileLocation, FileMode.Open, FileAccess.Read))
+            {
+                hssfwb = new XSSFWorkbook(file);
+            }
+
+            return hssfwb;
+        }
+
+        public XSSFWorkbook EditXLSX(XSSFWorkbook workbook)
+        {
+            ISheet sheet1 = workbook.GetSheet("Sheet1");
+            sheet1.CreateRow(0).CreateCell(0).SetCellValue("This is a Sample");
+            return workbook;
+        }
+
+        public void WriteXLSX(XSSFWorkbook workbook, string filename)
+        {
+            string fileLocation = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + filename;
+            FileStream file = new FileStream(@fileLocation, FileMode.Create);
+            workbook.Write(file);
+            file.Close();
         }
     }
 }
